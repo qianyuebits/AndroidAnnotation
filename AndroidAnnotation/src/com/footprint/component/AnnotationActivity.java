dfa337f0f1d6f023b6ba7eaf1e1c8adfb1308705
footprint <lilinquan1990@gmail.com>
2014-06-21 18:58:59 +0800

    第一步：实现一个可以注解Activity属性的Activity

---
+++ AndroidAnnotation/src/com/footprint/component/AnnotationActivity.java
@@ -0,0 +1,16 @@
+package com.footprint.component;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * TODO add doc
+ * */
+public class AnnotationActivity extends Activity{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+	}
+}
